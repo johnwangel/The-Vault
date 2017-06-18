e7d5033f8ee336c6c7d7013b8fb51f0017c94ca0
John Atkins <johnatkins1999@yahoo.com>
2017-06-17 15:36:51 -1000

    first iteration

--- vault.js
+++ vault.js
@@ -1,4 +1,19 @@
 'use strict';
 module.exports = function() {
-  
+
+  let vault = {};
+
+  module.setValue = function( key, value ){
+    vault[key]  = value;
+  }
+
+  module.getValue = function ( key ){
+    if (vault.hasOwnProperty(key)){
+      return vault[key];
+    } else { return null; }
+  }
+
+  return module;
+
 };
+
